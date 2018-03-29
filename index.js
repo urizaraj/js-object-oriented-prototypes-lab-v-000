fe22ca771b8baed4217f4fb0d828c8e21a8ef0b6
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 19:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
+}
 
-  this.veto = function(){
-    return 'No, I must disagree'
-  }
-  this.approve = function(){
-    return 'You can do that!'
-  }
-  this.doCharity = function(){
-    return "I like to help people."
-  }
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
+}
+BoardMember.prototype.approve = function(){
+  return 'You can do that!'
+}
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people."
+}
 
-  this.releasePressStatement = function(){
-    return "You will see great things from Scuber."
-  }
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
 
-  this.sayHi = function(){
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  }
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
